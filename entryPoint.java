e99eceb7863477302545b3f97ac8a90ab15e5064
REYNEP <parkeramitrakshar@gmail.com>
2025-10-23 09:34:57 +0600

    💁‍♀️ 1. Created Window (Platform-Independent) 😄 https://youtube.com/watch?v=x4jRPwAgwzA

---
+++ entryPoint.java
@@ -0,0 +1,22 @@
+import java.awt.event.WindowEvent;
+
+public class entryPoint {
+    public static void main(String[] argumentVec) 
+    {
+        java.awt.event.WindowListener EventKonsument1 = new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        /**
+         * AWT Provides it's own EventConsumers(win32 key/mouse/HCI events) + TheMainLoop
+         */
+
+        java.awt.Frame  window1 = new java.awt.Frame("White AWT Window");
+                        window1.setSize(1080, 1080);
+                        window1.setBackground(java.awt.Color.WHITE);
+                        window1.addWindowListener(EventKonsument1);
+                        window1.setVisible(true);
+    }
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+<h1  style="margin: 0; padding: 0"                           >  cse110: Java 1              </h1>
+<div style="font-size: 1.5em; margin-top: -7px;" align=center>  (Fall 2025)                 </div>
+<div style="font-size: 1em;   margin-top:    0;" align=center>  @ BRAC University Bangladesh</div>
+
+## Goals
+- Gradient Triangle (Vulkan)
+
+## Course Detes
+- Outline: `./0.outline/`
+- PDF notes: `./1.notes/`
+- Slides: They are locked using `e/2PACX-` 
+    - i.e. https://docs.google.com/presentation/d/e/2PACX-(extra-identification-letters)
+- Lecture Videos
+    - Bangla:  https://www.youtube.com/playlist?list=PLvr0Ht-XkB_0KC2-N3hv0V3ib-Z6wKkAy
+    - English: https://www.youtube.com/playlist?list=PLr9he3UyIy3mOj-ehFpODvCS1U2d2RtRQ
