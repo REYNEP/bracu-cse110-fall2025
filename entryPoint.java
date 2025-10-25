ab5c4938e4f2f7210389ff8413e06665c0f9be55
REYNEP <parkeramitrakshar@gmail.com>
2025-10-25 12:28:58 +0600

    🎴 4. making java look prettier 🎇 matthewthorning.align-vertically www.youtube.com/watch?v=e2vwoygtb3g

--- entryPoint.java
+++ entryPoint.java
@@ -1,6 +1,7 @@
 import java.awt.event.WindowEvent;
 
 import org.lwjgl.vulkan.*;
+import org.lwjgl.vulkan.VK10.*;
 import org.lwjgl.system.MemoryStack;
 
 public class entryPoint {
@@ -17,20 +18,23 @@ public class entryPoint {
          */
 
         java.awt.Frame  window1 = new java.awt.Frame("White AWT Window");
-                        window1.setSize(1080, 1080);
-                        window1.setBackground(java.awt.Color.WHITE);
-                        window1.addWindowListener(EventKonsument1);
-                        window1.setVisible(true);
+                        window1.setSize          (1080, 1080          );
+                        window1.setBackground    (java.awt.Color.WHITE);
+                        window1.addWindowListener(EventKonsument1     );
+                        window1.setVisible       (true                );
+        /**
+         * Aligning:- matthewthorning.align-vertically --> REYNEP's ShortCut ==> "alt+q"
+         */
 
         try(org.lwjgl.system.MemoryStack mStack               = MemoryStack.stackPush()) {
             VkApplicationInfo            amVK_ApplicationInfo = VkApplicationInfo.calloc(mStack)
-                .pNext(0)
-                .sType(             VK10.VK_STRUCTURE_TYPE_APPLICATION_INFO)
-                .pApplicationName(  mStack.UTF8("amVK_ApplicationInfo.pApplicationName not given"))
-                .pEngineName(       mStack.UTF8("amVK_ApplicationInfo.pEngineName not given"))
-                .applicationVersion(VK10.VK_MAKE_API_VERSION(0, 0, 0, 0))
-                .engineVersion(     VK10.VK_MAKE_API_VERSION(0, 0, 0, 0))
-                .apiVersion(        VK10.VK_API_VERSION_1_0);
+                .sType             (VK10.VK_STRUCTURE_TYPE_APPLICATION_INFO                                   )
+                .pNext             (0                                                                         )
+                .pApplicationName  (mStack.UTF8             ("amVK_ApplicationInfo.pApplicationName not given"))
+                .applicationVersion(VK10.VK_MAKE_API_VERSION(0, 0, 0, 0                                       ))
+                .pEngineName       (mStack.UTF8             ("amVK_ApplicationInfo.pEngineName not given"     ))
+                .engineVersion     (VK10.VK_MAKE_API_VERSION(0, 0, 0, 0                                       ))
+                .apiVersion        (VK10.VK_API_VERSION_1_0                                                   );
         }
     }
 };
